ff1c1ce2330e63e178df93f0f3d9009e957756ad
Carlos <carlos@stackfive.io>
2024-04-22 16:03:23 -0400

    test

--- bin/validate.js
+++ bin/validate.js
@@ -1,1 +1,38 @@
-console.log("process args: ", process.argv)
+const { readFile, stat } = require("fs/promises");
+const path = require("path");
+const glob = require("tiny-glob");
+const fetch = require("node-fetch");
+
+const changedFiles = process.argv[2];
+
+let passedValidation = true;
+for (const changedFile of changedFiles) {
+  if (changedFile.startsWith("_data/handles")) {
+    // handle handles file validation
+    const blob = await readFile("./" + handleFile);
+    let parsedHandle;
+    try {
+      parsedHandle = JSON.parse(blob);
+    } catch (err) {
+      console.error(`Unable to parse JSON file at ${handleFile}`);
+      passedValidation = false;
+      continue;
+    }
+    if (Object.prototype.hasOwnProperty.call(parsedHandle, "members")) {
+      Array.prototype.forEach.call(parsedHandle.members, (member) => {
+        if (!uniqueHandles.has(member)) {
+          console.error(
+            `Team specified in ${handleFile} has unregistered handle: ${member}`
+          );
+          passedValidation = false;
+        }
+      });
+    } else {
+      console.log("no members");
+      const res = await fetch(`https://main--api-code4rena-com.netlify.app/api/get-team?id=karl`)
+      console.log(res)
+    }
+  } else if (changedFile.startsWith("_data/orgs")) {
+    // handle orgs file validation
+  }
+}
