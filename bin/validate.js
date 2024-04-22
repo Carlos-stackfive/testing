2f59bff4760991dfdaa5ae603946aed47e6c5237
Carlos <carlos@stackfive.io>
2024-04-22 16:54:07 -0400

    test

--- bin/validate.js
+++ bin/validate.js
@@ -1,17 +1,15 @@
-const { readFile, stat } = require("fs/promises");
+const { readFile } = require("fs/promises");
 const fetch = require("node-fetch");
 
 // validate teams
 (async () => {
-  const [argv1, argv2, ...changedFiles] = process.argv; // single string or array of files changed
-  console.log("changedFiles: ", changedFiles)
+  const [argv1, argv2, ...changedFiles] = process.argv; // get all changed files
   let passedValidation = true;
-  for (const changedFile of changedFiles) {
-    console.log(changedFile)
+  for (const changedFile of changedFiles) { // for each changed file, do validation for team files
     if (!changedFile.startsWith("_data/handles")) {
       continue;
     }
-    // handle handles file validation
+    // read and parse the changed file
     const blob = await readFile("./" + changedFile);
     let parsedHandle;
     try {
@@ -21,17 +19,46 @@ const fetch = require("node-fetch");
       passedValidation = false;
       continue;
     }
-    if (Object.prototype.hasOwnProperty.call(parsedHandle, "members")) {
-      for (const member of parsedHandle.members) {
-        const res = await fetch(`https://api.code4rena.com/api/get-team?id=${member}`)
-        if (res.status !== 200) {
-          console.error(`Team member ${member} does not exist.`);
-          passedValidation = false;
-        }
-      }
-    } else {
+    // check that the required handle field exists
+    const teamHandle = parsedHandle.handle
+    if (!teamHandle) {
+      console.error("Handle field must exist.");
+      passedValidation = false;
+      continue;
+    }
+    // check that the required members field exists
+    const teamMembers = parsedHandle.members
+    if (!teamHandle) {
+      console.error("Members field must exist.");
+      passedValidation = false;
+      continue;
+    }
+    // check that there is at least 1 member
+    if (teamMembers.length < 1) {
       console.error("Team must have 1 or more members");
       passedValidation = false;
+      continue;
+    }
+    // check that handle is not a token handle (by team or user)
+    let res = await fetch(`https://api.code4rena.com/api/get-user?id=${teamHandle}`)
+    if (res.status === 200) {
+      console.error("Handle is taken.");
+      passedValidation = false;
+      continue;
+    }
+    res = await fetch(`https://api.code4rena.com/api/get-team?id=${teamHandle}`)
+    if (res.status === 200) {
+      console.error("Handle is taken.");
+      passedValidation = false;
+      continue;
+    }
+    // check that each member in the team exists
+    for (const member of parsedHandle.members) {
+      const res = await fetch(`https://api.code4rena.com/api/get-user?id=${member}`)
+      if (res.status !== 200) {
+        console.error(`Team member ${member} does not exist.`);
+        passedValidation = false;
+      }
     }
   }
   if (!passedValidation) {
