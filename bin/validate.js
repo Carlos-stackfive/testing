001a436a3bb4e6d3f367aae1d4f0a55531eb3d47
Carlos <carlos@stackfive.io>
2024-04-22 16:29:12 -0400

    test

--- bin/validate.js
+++ bin/validate.js
@@ -2,10 +2,15 @@ const { readFile, stat } = require("fs/promises");
 const path = require("path");
 const fetch = require("node-fetch");
 
-const changedFiles = process.argv[2];
-const fn = async () => {
+
+
+// validate teams
+(async () => {
+  const changedFiles = process.argv[2]; // array of files changed
+  console.log("changedFiles: ", changedFiles)
   let passedValidation = true;
   for (const changedFile of changedFiles) {
+    console.log(changedFile)
     if (changedFile.startsWith("_data/handles")) {
       // handle handles file validation
       const blob = await readFile("./" + handleFile);
@@ -37,10 +42,4 @@ const fn = async () => {
       // handle orgs file validation
     }
   }
-}
-(async () => {
-  const res = await fetch(`https://api.code4rena.com/api/get-team?id=karl`)
-  const res2 = await fetch(`https://api.code4rena.com/api/get-team?id=carlos_sf`)
-  console.log(res)
-  console.log(res2)
 })();
