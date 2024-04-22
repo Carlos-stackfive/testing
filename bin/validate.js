cd5b74d760acd33b63d62df24db077fc4318f4eb
Carlos <carlos@stackfive.io>
2024-04-22 16:34:25 -0400

    test

--- bin/validate.js
+++ bin/validate.js
@@ -1,8 +1,5 @@
 const { readFile, stat } = require("fs/promises");
-const path = require("path");
 const fetch = require("node-fetch");
-
-
 
 // validate teams
 (async () => {
@@ -11,35 +8,35 @@ const fetch = require("node-fetch");
   let passedValidation = true;
   for (const changedFile of changedFiles) {
     console.log(changedFile)
-    if (changedFile.startsWith("_data/handles")) {
-      // handle handles file validation
-      const blob = await readFile("./" + handleFile);
-      let parsedHandle;
-      try {
-        parsedHandle = JSON.parse(blob);
-      } catch (err) {
-        console.error(`Unable to parse JSON file at ${handleFile}`);
-        passedValidation = false;
-        continue;
-      }
-      if (Object.prototype.hasOwnProperty.call(parsedHandle, "members")) {
-        Array.prototype.forEach.call(parsedHandle.members, (member) => {
-          if (!uniqueHandles.has(member)) {
-            console.error(
-              `Team specified in ${handleFile} has unregistered handle: ${member}`
-            );
-            passedValidation = false;
-          }
-        });
-      } else {
-        console.log("no members");
-        const res = await fetch(`https://api.code4rena.com/api/get-team?id=karl`)
-        const res2 = await fetch(`https://api.code4rena.com/api/get-team?id=carlos_sf`)
-        console.log(res)
-        console.log(res2)
-      }
-    } else if (changedFile.startsWith("_data/orgs")) {
-      // handle orgs file validation
+    if (!changedFile.startsWith("_data/handles")) {
+      continue;
     }
+    // handle handles file validation
+    const blob = await readFile("./" + handleFile);
+    let parsedHandle;
+    try {
+      parsedHandle = JSON.parse(blob);
+    } catch (err) {
+      console.error(`Unable to parse JSON file at ${handleFile}`);
+      passedValidation = false;
+      continue;
+    }
+    if (Object.prototype.hasOwnProperty.call(parsedHandle, "members")) {
+      for (const member of parsedHandle.members) {
+        const res = await fetch(`https://api.code4rena.com/api/get-team?id=${member}`)
+        if (res.status !== 200) {
+          console.error(`Team member ${member} does not exist.`);
+          passedValidation = false;
+        }
+      }
+    } else {
+      console.error("Team must have 1 or more members");
+      passedValidation = false;
+    }
+  }
+  if (!passedValidation) {
+    throw new Error(
+      "❌  Team validation failed. See above log for more information."
+    );
   }
 })();
